1ef4acd36f43e1c16ffa3ea946a62593b8c6dfe1
TaranpalSingh18 <taranarora1470@gmail.com>
2026-04-03 23:33:18 +0530

    btn

---
+++ Frontend/autofill/src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = () =>{
+    return(
+        <div className="bg-red h-10px w-10px">
+            <input type="submit" placeholder="Submit Button"/>
+        </div>
+    )
+}
+
+export default Button
